5a29754b89f9c60fc4e04dfdabaf3c169405b31e
zeshi <zeshi.z@gmail.com>
2017-09-15 09:02:58 -0700

    dev

--- snobalio.py
+++ snobalio.py
@@ -223,6 +223,7 @@ class Snobal(object):
                                       i_inputs1=i_inputs1,
                                       i_inputs2=i_inputs2,
                                       i_precips=i_precips)
+        result[np.isnan(result)] = self._c_states[np.isnan(result)]
         self._c_states = result
         self._swe = np.column_stack((self._swe, self._c_states[0] * self._c_states[1]))
         self._c_swe = self._swe[:, -1]
